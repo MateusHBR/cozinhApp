6bee2739bb8e9a04c2af54a081a7796af317cc11
MateusHBR <mateusmorais.r@gmail.com>
2020-06-12 09:12:36 -0300

    adicionando um identificador e uma rota para o onclick

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,3 +1,4 @@
+import 'package:cozinhapp/view/product_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cozinhapp/model/meal.dart';
@@ -9,92 +10,104 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Container(
-      padding: EdgeInsets.only(
-          bottom: 5, right: size.width * 0.04, left: size.width * 0.04),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        elevation: 5,
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                  ),
-                  child: Image.network(
-                    meal.imageUrl,
-                    height: size.height * 0.25,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  right: 10,
-                  bottom: 10,
-                  child: Container(
-                    width: size.width * 0.6,
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      vertical: size.height * 0.02,
-                      horizontal: size.width * 0.04,
-                    ),
-                    child: Text(
-                      '${meal.title}',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 26,
-                        color: Colors.white,
-                      ),
-                      softWrap: true, //permitir quebra de linha
-                      overflow: TextOverflow.fade,
-                    ),
-                  ),
-                ),
-              ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductScreen(
+              meal: meal,
             ),
-            Padding(
-              padding: EdgeInsets.all(size.height * 0.02),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+            bottom: 5, right: size.width * 0.04, left: size.width * 0.04),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 5,
+          child: Column(
+            children: <Widget>[
+              Stack(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.schedule),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text("20 min"),
-                    ],
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      meal.imageUrl,
+                      height: size.height * 0.25,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.work),
-                      SizedBox(
-                        width: 6,
+                  Positioned(
+                    right: 10,
+                    bottom: 10,
+                    child: Container(
+                      width: size.width * 0.6,
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
                       ),
-                      Text("Fácil"),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.attach_money),
-                      SizedBox(
-                        width: 6,
+                      padding: EdgeInsets.symmetric(
+                        vertical: size.height * 0.02,
+                        horizontal: size.width * 0.04,
                       ),
-                      Text("Barato"),
-                    ],
+                      child: Text(
+                        '${meal.title}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                        ),
+                        softWrap: true, //permitir quebra de linha
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.all(size.height * 0.02),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("20 min"),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("Fácil"),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.attach_money),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("Barato"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
